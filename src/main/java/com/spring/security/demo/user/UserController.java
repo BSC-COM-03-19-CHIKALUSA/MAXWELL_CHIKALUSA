d07723eb4a7de17a33aa94a46f563a12934ff484
chikalusa <bsc-com-03-19@cc.ac.mw>
2022-10-07 09:56:32 +0100

    added executive_role and admin_role to the update method in the user controller class

--- src/main/java/com/spring/security/demo/user/UserController.java
+++ src/main/java/com/spring/security/demo/user/UserController.java
@@ -48,6 +48,7 @@ public class UserController {
     }
 
     @PutMapping("update-role/{userName}/{role}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_EXECUTIVE')")
     public ResponseEntity<ApiResponse> updateUserRole(@PathVariable("userName") String userName,
                                                       @PathVariable("role") String role,
                                                       Authentication currentUser){
